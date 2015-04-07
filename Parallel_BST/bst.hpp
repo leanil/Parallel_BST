513f244a8654ff1be2fb99b5f976a276ca8923c2
Leitereg András <leanil94@gmail.com>
2015-04-07 16:14:50 +0200

    red-black tree

--- Parallel_BST/bst.hpp
+++ Parallel_BST/bst.hpp
@@ -2,27 +2,35 @@
 #include <utility>
 #include <stdexcept>
 #include <iostream>
+#include <functional>
 
 template <class K, class V>
 class BST
 {
-public:
+private:
 
 	struct Node
 	{
-		Node(std::pair<K, V> val, std::shared_ptr<Node> parent = std::shared_ptr<Node>())
-			: key{ std::move(val.first) }, value{ std::move(val.second) }, parent{ std::move(parent) } {}
-		K key;
-		V value;
-		std::shared_ptr<Node> left_child, right_child;
+		Node(std::pair<K, V> val, typename BST::Node_ptr parent)
+			: val{ std::move(val) }, red{ true }, parent{ std::move(parent) } {}
+		K& key() { return val.first; }
+		V& value() { return val.second; }
+		std::pair<K, V> val;
+		bool red;
+		typename BST::Node_ptr left_child, right_child;
 		std::weak_ptr<Node> parent;
 	};
+
+	using Node_ptr = std::shared_ptr < Node > ;
+	using child_selector = std::function < Node_ptr&(Node_ptr) > ;
+
+public:
 
 	// Ineffective if K is not MoveConstructible.
 	BST(K lb, K ub) : lw_bound{ std::move(lb) }, up_bound{ std::move(ub) } {}
 
 	BST(const BST& other) :lw_bound{ other.lw_bound }, up_bound{ other.up_bound } {
-		deep_copy(root, other.root, std::shared_ptr<Node>());
+		deep_copy(root, other.root, Node_ptr());
 	}
 
 	BST(BST&& other) : lw_bound{ std::move(other.lw_bound) }, up_bound{ std::move(other.up_bound) }, root{ std::move(other.root) } {}
@@ -32,10 +40,8 @@ public:
 		return *this;
 	}
 
-	void swap(BST& other) {
-		std::swap(lw_bound, other.lw_bound);
-		std::swap(up_bound, other.up_bound);
-		std::swap(root, other.root);
+	bool empty() {
+		return !root;
 	}
 
 	void insert(std::pair<K, V> val) {
@@ -43,20 +49,33 @@ public:
 			throw std::out_of_range("key is out of boundaries");
 		}
 		if (!root) {
-			root = std::make_shared<Node>(std::move(val));
+			root = std::make_shared<Node>(std::move(val), Node_ptr());
+			root->red = false;
 			return;
 		}
-		std::shared_ptr<Node> parent, child{ root };
-		while (parent = child, child = choose_child(parent,val.first));
-		choose_child(parent, val.first) = std::make_shared<Node>(std::move(val), parent);
+		Node_ptr act, child{ root };
+		while (act = child, child = choose_child(act, val.first));
+		act = choose_child(act, val.first) = std::make_shared<Node>(std::move(val), act);
 
+		auto left_child = [] (const Node_ptr& parent) ->Node_ptr& {return parent->left_child; };
+		auto right_child = [] (const Node_ptr& parent) ->Node_ptr& {return parent->right_child; };
+
+		while (parent(act) && parent(act)->red) {
+			if (parent(act) == grand_parent(act)->left_child) {
+				restore(act, left_child, right_child);
+			}
+			else {
+				restore(act, right_child, left_child);
+			}
+		}
+		root->red = false;
 	}
 
-	std::shared_ptr<Node> find_nearest(const K& key) {
+	typename BST::Node_ptr find_nearest(const K& key) {
 		if (empty()) {
 			throw logic_error("find_nearest in empty tree");
 		}
-		std::shared_ptr<Node> less{ root }, more{ root }, it{ root };
+		Node_ptr less{ root }, more{ root }, it{ root };
 		while (it && it->key != key) {
 			(key < it->key ? more : less) = it;
 			it = choose_child(it, key);
@@ -65,8 +84,10 @@ public:
 		return key - less->key < more->key - key ? less : more; // TODO Abstract metrics
 	}
 
-	bool empty() {
-		return !root;
+	void swap(BST& other) {
+		std::swap(lw_bound, other.lw_bound);
+		std::swap(up_bound, other.up_bound);
+		std::swap(root, other.root);
 	}
 
 	template <class K, class V>
@@ -74,30 +95,80 @@ public:
 
 private:
 
-	std::shared_ptr<Node>& choose_child(std::shared_ptr<Node> parent, const K& key) {
-		return key < parent->key ? parent->left_child : parent->right_child;
+	Node_ptr& choose_child(const Node_ptr& parent, const K& key) {
+		return key < parent->key() ? parent->left_child : parent->right_child;
 	}
 
-	void traverse(std::shared_ptr<Node> root, std::ostream& out) const {
+	Node_ptr parent(const Node_ptr& child) {
+		return child ? child->parent.lock() : child;
+	}
+
+	Node_ptr grand_parent(const Node_ptr& child) {
+		return parent(child) ? parent(child)->parent.lock() : parent(child);
+	}
+
+	void traverse(Node_ptr root, std::ostream& out) const {
 		if (!root) { return; }
 		traverse(root->left_child, out);
-		out << root->value << " ";
+		print<K,V>(out,*root);
 		traverse(root->right_child, out);
 	}
 
-	void deep_copy(std::shared_ptr<Node>& target, std::shared_ptr<const Node> source, std::shared_ptr<Node> parent) {
+	void deep_copy(Node_ptr& target, std::shared_ptr<const Node> source, Node_ptr parent) {
 		if (!source) { return; }
-		target = std::make_shared<Node>(std::make_pair(source->key, source->value), std::move(parent));
+		target = std::make_shared<Node>(source->val, std::move(parent));
 		deep_copy(target->left_child, source->left_child, target);
 		deep_copy(target->right_child, source->right_child, target);
 	}
 
+	void restore(Node_ptr& child, const child_selector& side1_child, const child_selector& side2_child) {
+		auto uncle = side2_child(grand_parent(child));
+		if (uncle && uncle->red) {
+			parent(child)->red = false;
+			uncle->red = false;
+			grand_parent(child)->red = true;
+			child = grand_parent(child);
+		}
+		else {
+			if (child == side2_child(parent(child))) {
+				child = parent(child);
+				rotate(child, side1_child, side2_child);
+			}
+			parent(child)->red = false;
+			grand_parent(child)->red = true;
+			rotate(grand_parent(child), side2_child, side1_child);
+		}
+	}
+
+	void rotate(Node_ptr x, const child_selector& side1_child, const child_selector& side2_child) {
+		auto y = side2_child(x);
+		side2_child(x) = side1_child(y);
+		if (side1_child(y)) {
+			side1_child(y)->parent = x;
+		}
+		y->parent = x->parent;
+		(x->parent.lock() ?
+			(x == side1_child(parent(x)) ? side1_child(parent(x)) : side2_child(parent(x))) :
+			root) = y;
+		side1_child(y) = x;
+		x->parent = y;
+	}
+
 	K lw_bound, up_bound;
-	std::shared_ptr<Node> root;
+	Node_ptr root;
 };
 
 template <class K, class V>
 std::ostream& operator<<(std::ostream& out, const BST<K, V>& bst) {
 	bst.traverse(bst.root, out);
+	return out;
+}
+
+template <class K, class V>
+std::ostream& print(std::ostream& out,typename BST<K,V>::Node node) {
+	out << node.key() << " " << (node.left_child ? node.left_child->key() : -1) << " "
+		<< (node.right_child ? node.right_child->key() : -1) << " "
+		<< (node.parent.lock() ? node.parent.lock()->key() : -1) << " "
+		<< (node.red?"p":"f") << endl;
 	return out;
 }
--- Parallel_BST/main.cpp
+++ Parallel_BST/main.cpp
@@ -1,12 +1,14 @@
 #include "bst.hpp"
 #include <random>
+#include <chrono>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
 
 #pragma region basic functions
-	vector<int> data{ 45, 50, 25, 10, 5, 20, 15, 40, 30, 35, 200 };
+	/*vector<int> data{ 45, 50, 25, 10, 5, 20, 15, 40, 30, 35, 200 };
 	BST<int, int> m_bst(0, 100);
 	for (int d : data) {
 		try {
@@ -21,10 +23,10 @@ int main() {
 		 << m_bst.find_nearest(43)->key << " "
 		 << m_bst.find_nearest(27)->key << " "
 		 << m_bst.find_nearest(28)->key << " "
-		 << m_bst.find_nearest(15)->key << endl;
+		 << m_bst.find_nearest(15)->key << endl;*/
 #pragma endregion
 
-#pragma region automatic destructor (does not leak)
+#pragma region implicit destructor (does not leak)
 	/*uniform_int_distribution<> dist(0, 100);
 	default_random_engine rand;
 	for(;;){
@@ -36,13 +38,39 @@ int main() {
 #pragma endregion
 
 #pragma region Copy/Move constructor/assignment
-	BST<int, int> m_bst2(0,1);
+	/*BST<int, int> m_bst2(0,1);
 	m_bst2 = m_bst;
 	cout << m_bst << endl;
 	m_bst2.insert(make_pair(0, 0));
 	cout << m_bst << endl;
-	cout << m_bst2 << endl;
+	cout << m_bst2 << endl;*/
 #pragma endregion
 
+#pragma region insert benchmark
+	/*BST<int, int> deformed(0, 10000);
+	auto start = chrono::high_resolution_clock::now();
+	for (int i = 0; i < 10000; i++) {
+		deformed.insert(make_pair(i, i));
+	}
+	auto end = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+	cout << duration << endl;*/
+#pragma endregion
+
+// TODO run when iterators are done
+#pragma region sorting check 
+	/*uniform_int_distribution<> dist(0, 1000000);
+	default_random_engine rand;
+	BST<int, int> m_bst(0, 1000000);
+	vector<int> values;
+	for (int i = 0; i < 10000; i++) {
+		int x = dist(rand);
+		m_bst.insert(make_pair(x, 0));
+		values.push_back(x);
+	}
+	sort(values.begin(), values.end());
+	vector<int> bst_values(m_bst.begin(), m_bst.end());
+	cout << (bst_values == values);*/
+#pragma endregion
 	return 0;
 }
