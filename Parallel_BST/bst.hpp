8583522ed9c06a208fec9a820dd71146bbc328a7
Leitereg András <leanil94@gmail.com>
2015-04-08 16:33:19 +0200

    iterator

--- Parallel_BST/bst.hpp
+++ Parallel_BST/bst.hpp
@@ -7,7 +7,7 @@
 #include <thread>
 #include <algorithm>
 
-template <class K, class V>
+template <class K, class V, class Compare = std::less<K>, class Distance = std::minus<K>>
 class BST
 {
 private:
@@ -16,8 +16,8 @@ private:
 
 	struct Node
 	{
-		Node(std::pair<K, V> val, typename BST::Node_ptr parent)
-			: val{ std::move(val) }, red{ true }, parent{ std::move(parent) } {}
+		Node(std::pair<K, V> val, typename BST::Node_ptr parent, bool red = true)
+			: val{ std::move(val) }, red{ red }, parent{ std::move(parent) } {}
 		K& key() { return val.first; }
 		V& value() { return val.second; }
 		std::pair<K, V> val;
@@ -31,28 +31,130 @@ private:
 
 public:
 
-	// Ineffective if K is not MoveConstructible.
-	BST(K lb, K ub) : lw_bound{ std::move(lb) }, up_bound{ std::move(ub) } {}
+	class BST_const_iterator;
 
-	BST(const BST& other) :lw_bound{ other.lw_bound }, up_bound{ other.up_bound } {
+	typedef std::pair<K, V> value_type;
+	typedef	BST_const_iterator iterator;
+	typedef iterator const_iterator;
+	typedef value_type& reference;
+	typedef const value_type& const_reference;
+	typedef value_type* pointer;
+	typedef int difference_type;
+	typedef unsigned size_type;
+
+	class BST_const_iterator
+	{
+	public:
+
+		typedef std::bidirectional_iterator_tag iterator_category;
+		typedef typename BST::value_type value_type;
+		typedef typename BST::difference_type difference_type;
+		typedef typename BST::pointer pointer;
+		typedef typename BST::reference reference;
+
+		BST_const_iterator() {}
+		BST_const_iterator(Node_ptr ptr) :ptr{ ptr } {}
+		BST_const_iterator(const BST_const_iterator&) = default;
+		BST_const_iterator(BST_const_iterator&& other) :ptr{ std::move(other.ptr) } {}
+
+		value_type operator*() {
+			return ptr->val;
+		}
+
+		const pointer operator->() {
+			return &ptr->val;
+		}
+
+		BST_const_iterator& operator++() {
+			if (!ptr);
+			else if (ptr->right_child) {
+				for (ptr = ptr->right_child; ptr->left_child; ptr = ptr->left_child);
+			}
+			else {
+				for (Node_ptr par; (par = parent(ptr)) && ptr == par->right_child; ptr = par);
+				ptr = parent(ptr);
+			}
+			return *this;
+		}
+
+		BST_const_iterator& operator++(int) {
+			BST_const_iterator tmp = *this;
+			++*this;
+			return tmp;
+		}
+
+		BST_const_iterator& operator--() {
+			if (!ptr) {
+				for (ptr = root; ptr->right_child; ptr = ptr->right_child);
+			}
+			else if (ptr->left_child) {
+				for (ptr = ptr->left_child; ptr->right_child; ptr = ptr->right_child);
+			}
+			else {
+				for (Node_ptr par; par = parent(ptr) && ptr == par->left_child; ptr = par);
+				ptr = parent(ptr);
+			}
+			return *this
+		}
+
+		BST_const_iterator& operator--(int) {
+			BST_const_iterator tmp = *this;
+			--*this;
+			return tmp;
+		}
+
+		bool operator==(const BST_const_iterator& other) {
+			return ptr == other.ptr;
+		}
+
+		bool operator!=(const BST_const_iterator& other) {
+			return !(*this == other);
+		}
+
+	private:
+		Node_ptr ptr;
+	};
+
+	BST(K lb, K ub) : lw_bound{ std::move(lb) }, up_bound{ std::move(ub) }, act_size{ 0 } {}
+
+	BST(const BST& other) :lw_bound{ other.lw_bound }, up_bound{ other.up_bound }, act_size{ other.act_size } {
 		deep_copy(root, other.root, Node_ptr());
 	}
 
-	BST(BST&& other) : lw_bound{ std::move(other.lw_bound) }, up_bound{ std::move(other.up_bound) }, root{ std::move(other.root) } {}
+	BST(BST&& other) : lw_bound{ std::move(other.lw_bound) }, up_bound{ std::move(other.up_bound) },
+		act_size{ other.act_size }, root{ std::move(other.root) } {}
+
+	template<class InputIt>
+	BST(K lb, K ub, InputIt first, InputIt last) : BST(lb, ub) {
+		std::for_each(first, last, [=] (const_reference val) {insert(val); });
+	}
 
 	BST& operator=(BST other) {
 		swap(other);
 		return *this;
 	}
 
+	iterator begin() {
+		Node_ptr tmp;
+		for (tmp = root; tmp->left_child; tmp = tmp->left_child);
+		return iterator{ tmp };
+	}
+
+	iterator end() {
+		return iterator();
+	}
+
+	size_type size() {
+		return act_size;
+	}
+
 	bool empty() {
-		return !root;
+		return act_size == 0;
 	}
 
 	void insert(std::pair<K, V> val) {
-		if (val.first < lw_bound || val.first >= up_bound) {
-			throw std::out_of_range("key is out of boundaries");
-		}
+		if (Compare()(val.first, lw_bound) || !Compare()(val.first, up_bound)) { return; }
+		act_size++;
 		if (!root) {
 			root = std::make_shared<Node>(std::move(val), Node_ptr());
 			root->red = false;
@@ -66,7 +168,7 @@ public:
 		auto right_child = [] (const Node_ptr& parent) ->Node_ptr& {return parent->right_child; };
 
 		while (parent(act) && parent(act)->red) {
-			if (parent(act) == grand_parent(act)->left_child) {
+			if (parent(act) == grand_parent(act)->left_child) { //2. körben nincs nagyapja
 				restore(act, left_child, right_child);
 			}
 			else {
@@ -76,81 +178,98 @@ public:
 		root->red = false;
 	}
 
-	typename BST::Node_ptr find_nearest(const K& key) {
+	iterator find_nearest(const K& key) {
 		if (empty()) {
 			throw logic_error("find_nearest in empty tree");
 		}
 		Node_ptr less{ root }, more{ root }, it{ root };
 		while (it && it->key() != key) {
-			(key < it->key() ? more : less) = it;
+			(Compare()(key, it->key()) ? more : less) = it;
 			it = choose_child(it, key);
 		}
 		if (it) { return it; }
-		return key - less->key() < more->key() - key ? less : more; // TODO Abstract metrics
+		return iterator(Compare()(Distance()(key, less->key()), Distance()(more->key(), key)) ? less : more);
 	}
 
 	template<class Range>
-	std::vector<typename BST::Node_ptr> find_nearest(const Range& rng) {
+	std::vector<iterator> find_nearest(const Range& rng) {
 		return find_nearest(rng.begin(), rng.end());
 	}
 
 	template<class InputIt>
-	std::vector<typename BST::Node_ptr> find_nearest(InputIt first, InputIt last) {
+	std::vector<iterator> find_nearest(InputIt first, InputIt last) {
 		if (empty()) {
 			throw logic_error("find_nearest in empty tree");
 		}
 		auto size = std::distance(first, last);
-		std::vector<typename BST::Node_ptr> result(size);
+		std::vector<iterator> result(size);
 		std::vector<std::thread> threads;
 
-		for (int i = 0; i < core_num-1; i++) {
+		for (int i = 0; i < core_num - 1; i++) {
 			auto from = first;
-			std::advance(first, size/core_num);
+			std::advance(first, size / core_num);
 			threads.push_back(std::thread{
 				[=, &result]() mutable {std::transform(from, first, result.begin() + i*(size / core_num),
-														[=] (const K& key) {return find_nearest(key); }); } });
+				[=] (const K& key) {return find_nearest(key); }); } });
 		}
 		threads.push_back(std::thread{
-			[=, &result]() mutable {std::transform(first, last, result.begin() + (core_num-1)*(size / core_num),
-														[=] (const K& key) {return find_nearest(key); }); } });
-		
-		for (auto& t : threads) { t.join(); }
-		return result;
+			[=, &result]() mutable {std::transform(first, last, result.begin() + (core_num - 1)*(size / core_num),
+			[=] (const K& key) {return find_nearest(key); }); } });
+
+			for (auto& t : threads) { t.join(); }
+			return result;
+	}
+
+	BST& change_bounds(K lb, K ub) {
+		bool shorter = Compare()(lw_bound, lb) || Compare()(ub, up_bound);
+		if (shorter) {
+			return *this = BST(lb, ub, begin(), end());
+		}
+		lw_bound = lb, up_bound = ub;
+		return *this;
 	}
 
 	void swap(BST& other) {
 		std::swap(lw_bound, other.lw_bound);
 		std::swap(up_bound, other.up_bound);
 		std::swap(root, other.root);
+		std::swap(act_size, other.act_size);
+	}
+
+	template<class UnaryFunction>
+	void recursive_for_each(UnaryFunction f) {
+		recursive_for_each(root, f);
 	}
 
 	template <class K, class V>
 	friend std::ostream& operator<<(std::ostream& out, const BST<K, V>& bst);
 
+
 private:
 
-	Node_ptr& choose_child(const Node_ptr& parent, const K& key) {
-		return key < parent->key() ? parent->left_child : parent->right_child;
+	static Node_ptr& choose_child(const Node_ptr& parent, const K& key) {
+		return Compare()(key, parent->key()) ? parent->left_child : parent->right_child;
 	}
 
-	Node_ptr parent(const Node_ptr& child) {
+	static Node_ptr parent(const Node_ptr& child) {
 		return child ? child->parent.lock() : child;
 	}
 
-	Node_ptr grand_parent(const Node_ptr& child) {
+	static Node_ptr grand_parent(const Node_ptr& child) {
 		return parent(child) ? parent(child)->parent.lock() : parent(child);
 	}
 
-	void traverse(Node_ptr root, std::ostream& out) const {
+	template<class UnaryFunction>
+	void recursive_for_each(Node_ptr root, UnaryFunction f) {
 		if (!root) { return; }
-		traverse(root->left_child, out);
-		print<K, V>(out, *root);
-		traverse(root->right_child, out);
+		recursive_for_each(root->left_child, f);
+		f(root->val);
+		recursive_for_each(root->right_child, f);
 	}
 
 	void deep_copy(Node_ptr& target, std::shared_ptr<const Node> source, Node_ptr parent) {
 		if (!source) { return; }
-		target = std::make_shared<Node>(source->val, std::move(parent));
+		target = std::make_shared<Node>(source->val, std::move(parent), source->red);
 		deep_copy(target->left_child, source->left_child, target);
 		deep_copy(target->right_child, source->right_child, target);
 	}
@@ -190,19 +309,11 @@ private:
 
 	K lw_bound, up_bound;
 	Node_ptr root;
+	size_type act_size;
 };
 
 template <class K, class V>
-std::ostream& operator<<(std::ostream& out, const BST<K, V>& bst) {
-	bst.traverse(bst.root, out);
-	return out;
-}
-
-template <class K, class V>
-std::ostream& print(std::ostream& out, typename BST<K, V>::Node node) {
-	out << node.key() << " " << (node.left_child ? node.left_child->key() : -1) << " "
-		<< (node.right_child ? node.right_child->key() : -1) << " "
-		<< (node.parent.lock() ? node.parent.lock()->key() : -1) << " "
-		<< (node.red ? "p" : "f") << endl;
+std::ostream& operator<<(std::ostream& out, BST<K, V>& bst) {
+	bst.recursive_for_each([&] (std::pair<K, V> val) { out << "{" << val.first << "," << val.second << "} "; });
 	return out;
 }
--- Parallel_BST/main.cpp
+++ Parallel_BST/main.cpp
@@ -6,62 +6,64 @@
 using namespace std;
 
 int main() {
-
+	BST<int, int> m_bst(0, 0);
 #pragma region basic functions
-	/*vector<int> data{ 45, 50, 25, 10, 5, 20, 15, 40, 30, 35, 200 };
-	BST<int, int> m_bst(0, 100);
+	vector<int> data{ 45, 50, 25, 10, 5, 20, 15, 40, 30, 35, 200 };
+	m_bst = BST<int, int>(0, 100);
 	for (int d : data) {
-		try {
-			m_bst.insert(make_pair(d, d));
-		}
-		catch (out_of_range e) {
-			cerr << "error inserting {" << d << "," << d << "}: " << e.what() << endl;
-		}
+		m_bst.insert(make_pair(d, d));
 	}
 
-	cout << m_bst.find_nearest(42)->key << " "
-		 << m_bst.find_nearest(43)->key << " "
-		 << m_bst.find_nearest(27)->key << " "
-		 << m_bst.find_nearest(28)->key << " "
-		 << m_bst.find_nearest(15)->key << endl;*/
+	cout << m_bst.find_nearest(42)->first << " "
+		<< m_bst.find_nearest(43)->first << " "
+		<< m_bst.find_nearest(27)->first << " "
+		<< m_bst.find_nearest(28)->first << " "
+		<< m_bst.find_nearest(15)->first << endl;
+	cout << m_bst << endl;
+	m_bst.change_bounds(15, 40);
+	for (auto it : m_bst) {
+		cout << "{" << it.first << "," << it.second << "} ";
+	}
+	cout << endl << endl;
 #pragma endregion
 
 #pragma region implicit destructor (does not leak)
 	/*uniform_int_distribution<> dist(0, 100);
 	default_random_engine rand;
 	for(;;){
-		BST<long long, long long> huge_bst(0,100);
-		for (int i = 0; i < 1000; i++) {
-			huge_bst.insert(make_pair(dist(rand), i));
-		}
+	BST<long long, long long> huge_bst(0,100);
+	for (int i = 0; i < 1000; i++) {
+	huge_bst.insert(make_pair(dist(rand), i));
+	}
 	}*/
 #pragma endregion
 
 #pragma region Copy/Move constructor/assignment
-	/*BST<int, int> m_bst2(0,1);
+	BST<int, int> m_bst2(0, 1);
 	m_bst2 = m_bst;
+	m_bst2.insert(make_pair(39, 39));
 	cout << m_bst << endl;
-	m_bst2.insert(make_pair(0, 0));
-	cout << m_bst << endl;
-	cout << m_bst2 << endl;*/
+	cout << m_bst2 << endl;
+	m_bst = move(m_bst2);
+	cout << m_bst2 << endl;
+	cout << m_bst << endl << endl;
 #pragma endregion
 
 #pragma region insert benchmark
-	/*BST<int, int> deformed(0, 10000);
-	auto start = chrono::high_resolution_clock::now();
+	BST<int, int> deformed(0, 10000);
+	auto start1 = chrono::high_resolution_clock::now();
 	for (int i = 0; i < 10000; i++) {
 		deformed.insert(make_pair(i, i));
 	}
-	auto end = chrono::high_resolution_clock::now();
-	auto duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
-	cout << duration << endl;*/
+	auto end1 = chrono::high_resolution_clock::now();
+	auto duration1 = chrono::duration_cast<chrono::milliseconds>(end1 - start1).count();
+	cout << duration1 << endl << endl;
 #pragma endregion
 
-// TODO run when iterators are done
 #pragma region sorting check 
-	/*uniform_int_distribution<> dist(0, 1000000);
+	uniform_int_distribution<> dist(0, 1000000);
 	default_random_engine rand;
-	BST<int, int> m_bst(0, 1000000);
+	m_bst = BST<int, int>(0, 1000000);
 	vector<int> values;
 	for (int i = 0; i < 10000; i++) {
 		int x = dist(rand);
@@ -69,22 +71,23 @@ int main() {
 		values.push_back(x);
 	}
 	sort(values.begin(), values.end());
-	vector<int> bst_values(m_bst.begin(), m_bst.end());
-	cout << (bst_values == values);*/
+	vector<int> bst_values(values.size());
+	transform(m_bst.begin(), m_bst.end(), bst_values.begin(), [] (pair<int, int> p) {return p.first; });
+	cout << (bst_values == values) << endl << endl;
 #pragma endregion
 
 #pragma region parallel find
-	BST<int, int> m_bst(0, 1000000);
-	vector<int> query(1000000);
-	for (int i = 0; i < 1000000; i++) {
+	m_bst = BST<int, int>(0, 100000);
+	vector<int> query(100000);
+	for (int i = 0; i < 100000; i++) {
 		query[i] = i;
 		m_bst.insert(make_pair(i, i));
 	}
-	auto start = chrono::high_resolution_clock::now();
+	auto start2 = chrono::high_resolution_clock::now();
 	auto result = m_bst.find_nearest(query);
-	auto end = chrono::high_resolution_clock::now();
-	auto duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
-	cout << duration << " " << result[0]->key() << endl;
+	auto end2 = chrono::high_resolution_clock::now();
+	auto duration2 = chrono::duration_cast<chrono::milliseconds>(end2 - start2).count();
+	cout << duration2 << " " << result[0]->first << endl;
 
 #pragma endregion
 	return 0;
